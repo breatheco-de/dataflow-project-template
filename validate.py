562be226fe41c6e250be0e41347d8ce1e33e1cc3
Alejandro Sanchez <aalejo@gmail.com>
2022-09-07 18:48:17 -0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,27 +2,48 @@ import random
 import sys
 import pandas as pd
 import os
+import traceback
+
 
 from colorama import Fore, Back, Style
 from utils.core import (
-    load_pipelines_from_project, get_params, get_transformation, scan_for_pipelines, 
+    load_pipelines_from_project, get_params, get_transformation, scan_for_pipelines,
     scan_pipeline_transformations
 )
 from deepdiff import DeepDiff
 
 
+def to_df(df):
+    if isinstance(df, list):
+        try:
+            return pd.DataFrame(df)
+        except Exception as e:
+            print(df)
+            raise e
+    elif isinstance(df, pd.DataFrame):
+        return df
+    else:
+        raise Exception(
+            f'Invalid type {type(df)} for variable "expected_input" or "ouput" (expected list or DataFrame)')
+
+
+def lo_list(df):
+    if isinstance(df, list):
+        return df
+    elif isinstance(df, pd.DataFrame):
+        return df.to_dict('records')
+    else:
+        raise Exception(
+            f'Invalid type {type(df)} for variable "expected_input" or "ouput" (expected list or DataFrame)')
+
+
 def validate_trans(q, t, _errors):
     try:
         run, _in, _out = get_transformation(q, t)
-        if isinstance(_in, list):
-            output = run(pd.DataFrame(_in, index=[0])).to_dict('records')
-        elif isinstance(_id, pd.DataFrame):
-            output = run(_in).to_dict('records')
-        else:
-            raise Exception(f'Invalid type {type(_id)} for variable expected_input (expected list or DataFrame)')
+        output = run(to_df(_in)).to_dict('records')
 
-        in_out_same = DeepDiff(_in, _out)
-        diff = DeepDiff(output, _out)
+        in_out_same = DeepDiff(lo_list(_in), lo_list(_out))
+        diff = DeepDiff(lo_list(output), lo_list(_out))
     except Exception as e:
         _errors[q + '.' + t] = e
 
@@ -61,10 +82,14 @@ if len(errors) > 0:
         count += 1
         print(Fore.RED + f'{count}) In {e}: ', end='')
         print(errors[e])
+        if not isinstance(errors[e], str):
+            traceback.print_exception(
+                type(errors[e]), errors[e], errors[e].__traceback__)
 
     print(Style.RESET_ALL + '\n')
     exit(1)
 else:
-    print(Back.GREEN + f'Report: All {len(pipelines)} transformations return the expected outputs 🙂', end='')
+    print(Back.GREEN +
+          f'Report: All {len(pipelines)} transformations return the expected outputs 🙂', end='')
     print(Style.RESET_ALL)
     exit(0)
